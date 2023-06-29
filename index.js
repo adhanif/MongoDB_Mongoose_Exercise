f5818ac0a960529ad9ec1723fe75c39999f93c0c
ad.hanif <ad.hanif@outlook.com>
2023-06-29 13:19:41 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const port = 3000;
+const app = express();
+const URL = process.env.URL;
+
+//Initial connecting
+mongoose
+  .connect(URL)
+  .then(() => console.log("connected to db"))
+  .catch((err) => {
+    console.log("error connecting to db", err);
+  });
+
+//After Initial connecting
+
+mongoose.connection.on("error", (err) => {
+  console.log("lost connection", err);
+});
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+// app.listen(port, () => {
+//   console.log(`Example app listening on port http://localhost:${port}`);
+// });
